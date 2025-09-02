671acb016103555789f3ac5160b1db6a39c674eb
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-01 22:26:56 -0700

    update Navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from '../button/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [Button],
   templateUrl: './navbar.html',
 })
 export class Navbar {}
