93889547a379d3e17e575e8a36323fa70b0d9f05
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-04 12:04:24 -0700

    create accordion for sidebar

--- src/app/pages/boards/boards.ts
+++ src/app/pages/boards/boards.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 import {
   faBox,
   faWaveSquare,
   faClock,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faGear,
 } from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-boards',
-  imports: [CommonModule, Navbar, FontAwesomeModule],
+  imports: [CommonModule, Navbar, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.html',
 })
 export class Boards {
@@ -20,4 +27,10 @@ export class Boards {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
 }
