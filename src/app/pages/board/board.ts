11d699bcdd2b5f2fe31e4baaa46b7fb5a12c191b
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-04 16:15:19 -0700

    Add Drag and Drop Event

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,9 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  DragDropModule,
+  CdkDragDrop,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { ToDo } from '../../models/todo.model';
 import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-board',
-  imports: [Navbar],
+  imports: [CommonModule, DragDropModule, Navbar],
   templateUrl: './board.html',
 })
-export class Board {}
+export class Board {
+  todos: ToDo[] = [
+    {
+      id: '1',
+      title: 'Make dishes',
+    },
+    {
+      id: '2',
+      title: 'Buy a unicorn',
+    },
+    {
+      id: '2',
+      title: 'Watch Angular Path in Platzi',
+    },
+  ];
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+  }
+}
