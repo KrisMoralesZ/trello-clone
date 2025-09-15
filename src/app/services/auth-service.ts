170ef9730a6610de0906f6dbd9488001bc1003ba
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-15 13:44:11 -0700

    Create Auth Service

---
+++ src/app/services/auth-service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  apiUrl = 'https://fake-trello-api.herokuapp.com/';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
+      email,
+      password,
+    });
+  }
+}
