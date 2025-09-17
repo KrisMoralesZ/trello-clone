ba5040bdade22dcd624914836298fc6fa2dc565f
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-17 11:04:40 -0700

    add service

--- src/app/services/auth/auth-service.ts
+++ src/app/services/auth/auth-service.ts
@@ -15,4 +15,12 @@ export class AuthService {
       password,
     });
   }
+
+  signup(name: string, email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {
+      name,
+      email,
+      password,
+    });
+  }
 }
