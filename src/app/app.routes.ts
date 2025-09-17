80522f4a1ec3c225ea04aa56c1ee938fc1b01eab
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-17 09:05:36 -0700

    move to Auth folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
+import { Login } from '@pages/authentication/login/login';
 import { Boards } from './pages/boards/boards';
 import { Board } from './pages/board/board';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
   {
     path: 'login',
     component: Login,
