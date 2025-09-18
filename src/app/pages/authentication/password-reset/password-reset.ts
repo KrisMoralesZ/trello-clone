4fee8b93e1881d7de8b17d1a9d7f56985626a44f
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-17 20:14:12 -0700

    add password reset files

---
+++ src/app/pages/authentication/password-reset/password-reset.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Button } from '@components/button/button';
+
+@Component({
+  selector: 'app-password-reset',
+  imports: [Button],
+  templateUrl: './password-reset.html',
+})
+export class PasswordReset {}
