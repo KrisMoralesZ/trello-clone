63487f68c1bcb15d637906d7cbb0ef2783646d1d
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-17 20:40:50 -0700

    add Logic and refactor

--- src/app/pages/authentication/password-reset/password-reset.ts
+++ src/app/pages/authentication/password-reset/password-reset.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Button } from '@components/button/button';
+import { RequestStatus } from '@models/status.model';
+import { AuthService } from '@services/auth/auth-service';
 
 @Component({
   selector: 'app-password-reset',
-  imports: [Button],
+  imports: [Button, ReactiveFormsModule],
   templateUrl: './password-reset.html',
 })
-export class PasswordReset {}
+export class PasswordReset {
+  private authService: AuthService;
+  private formBuilder: FormBuilder;
+  form: ReturnType<FormBuilder['group']>;
+  status: RequestStatus = 'init';
+  emailSent = false;
+
+  constructor() {
+    const authService = inject(AuthService);
+    const formBuilder = inject(FormBuilder);
+
+    this.authService = authService;
+    this.formBuilder = formBuilder;
+    this.form = this.formBuilder.nonNullable.group({
+      email: ['', [Validators.email, Validators.required]],
+    });
+  }
+
+  sendLink() {
+    if (this.form.valid) {
+      this.status = 'loading';
+      const { email } = this.form.getRawValue();
+      this.authService.recovery(email).subscribe({
+        next: () => {
+          this.status = 'success';
+          this.emailSent = true;
+        },
+        error: () => {
+          this.status = 'failed';
+        },
+      });
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
