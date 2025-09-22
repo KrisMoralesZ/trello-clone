8d804122300541b112535a4c3307bbff9dfe4b56
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-22 16:53:41 -0700

    Create users Service

---
+++ src/app/services/users/users-service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '@models/user.model';
+import { apiUrl } from '@services/apiUrsl';
+import { AuthService } from '@services/auth/auth-service';
+import { BehaviorSubject, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private http = inject(HttpClient);
+  private autService = inject(AuthService);
+  private apiUrl = apiUrl;
+
+  user$ = new BehaviorSubject<User | null>(null);
+
+  getUserData() {
+    return this.user$.getValue();
+  }
+
+  getProfile() {
+    const token = this.autService.getToken();
+    return this.http
+      .get<User>(`${this.apiUrl}/api/v1/auth/profile`, {
+        headers: { Authorization: `Bearer ${token}` || '' },
+      })
+      .pipe(tap((user) => this.user$.next(user)));
+  }
+}
