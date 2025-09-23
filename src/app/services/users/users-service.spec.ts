8a47d6a74d922e720dca729f06d1cbcdcc240e52
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-22 18:11:48 -0700

    Fix Failing Test

--- src/app/services/users/users-service.spec.ts
+++ src/app/services/users/users-service.spec.ts
@@ -1,12 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users-service';
+import { AuthService } from '@services/auth/auth-service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let authService: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService],
+    });
     service = TestBed.inject(UsersService);
   });
 
