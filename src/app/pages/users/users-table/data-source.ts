046690aa95b4340a5aa0b52e7ec0976cb11fbe42
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-24 18:03:52 -0700

    Create Data Source and Service

---
+++ src/app/pages/users/users-table/data-source.ts
@@ -0,0 +1,32 @@
+import { DataSource } from '@angular/cdk/collections';
+import { User } from '@models/user.model';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export class DataSourceUser extends DataSource<User> {
+  data = new BehaviorSubject<User[]>([]);
+  originalData: User[] = [];
+
+  connect(): Observable<User[]> {
+    return this.data.asObservable();
+  }
+
+  disconnect(): void {
+    this.data.complete();
+  }
+
+  setData(data: User[]) {
+    this.originalData = data;
+    this.data.next(data);
+  }
+
+  filterData(filter: string) {
+    if (!filter) {
+      this.data.next(this.originalData);
+      return;
+    }
+    const filteredData = this.originalData.filter((user) =>
+      user.username.toLowerCase().includes(filter.toLowerCase())
+    );
+    this.data.next(filteredData);
+  }
+}
