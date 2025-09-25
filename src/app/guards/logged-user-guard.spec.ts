f815bbf110aa18107a29d26c69c7e5e5eba344f0
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-25 11:51:57 -0700

    Add Tests

--- src/app/guards/logged-user-guard.spec.ts
+++ src/app/guards/logged-user-guard.spec.ts
@@ -1,16 +1,52 @@
+import { AuthService } from '@services/auth/auth-service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { loggedUserGuard } from './logged-user-guard';
+import { HttpClient } from '@angular/common/http';
 
 describe('loggedUserGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => loggedUserGuard(...guardParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService, HttpClient],
+    });
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should return true if token exists', () => {
+    const mockAuthService = {
+      getToken: () => 'mock-token',
+    };
+    TestBed.overrideProvider('AuthService', { useValue: mockAuthService });
+
+    const mockRoute = {} as ActivatedRouteSnapshot;
+    const mockState = {} as RouterStateSnapshot;
+
+    const result = executeGuard(mockRoute, mockState);
+    expect(result).toBeTrue();
+  });
+
+  it('should return false if token does not exist', () => {
+    const mockAuthService = {
+      getToken: () => null,
+    };
+    TestBed.overrideProvider('AuthService', { useValue: mockAuthService });
+
+    const mockRoute = {} as ActivatedRouteSnapshot;
+    const mockState = {} as RouterStateSnapshot;
+
+    const result = executeGuard(mockRoute, mockState);
+    expect(result).toBeTrue();
   });
 });
