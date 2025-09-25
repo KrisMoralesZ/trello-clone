917d6c507faa94c97f6e2ebcba6b2e800769e9c1
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-09-25 13:27:25 -0700

    Fix test

--- src/app/pages/users/users-table/users-table.spec.ts
+++ src/app/pages/users/users-table/users-table.spec.ts
@@ -1,16 +1,34 @@
+import { UsersService } from '@services/users/users-service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersTable } from './users-table';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '@services/auth/auth-service';
+import { of } from 'rxjs';
 
 describe('UsersTable', () => {
   let component: UsersTable;
   let fixture: ComponentFixture<UsersTable>;
 
   beforeEach(async () => {
+    const usersServiceSpy = jasmine.createSpyObj('UsersService', [
+      'getUsers',
+      'getProfile',
+    ]);
+    usersServiceSpy.getUsers.and.returnValue(
+      of([{ id: 1, name: 'Test User', email: 'test@mail.com', avatar: 'url' }])
+    );
+    usersServiceSpy.getProfile.and.returnValue(
+      of({ id: 1, username: 'Tester' })
+    );
     await TestBed.configureTestingModule({
-      imports: [UsersTable]
-    })
-    .compileComponents();
+      imports: [UsersTable],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: usersServiceSpy },
+        HttpClient,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UsersTable);
     component = fixture.componentInstance;
