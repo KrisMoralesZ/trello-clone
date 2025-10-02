a88ee220a76f087cc92364fcc966ca94c92972ac
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-02 11:02:49 -0700

    Create New Board

--- src/app/components/new-board-form/new-board-form.ts
+++ src/app/components/new-board-form/new-board-form.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Colors } from '@models/colors.model';
+import { BoardsService } from '@services/boards/boards-service';
+import { Button } from '@components/button/button';
 
 @Component({
   selector: 'app-new-board-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, Button],
   templateUrl: './new-board-form.html',
 })
-export class NewBoardForm {}
+export class NewBoardForm {
+  private formBuilder = inject(FormBuilder);
+  private boardsService = inject(BoardsService);
+  private router = inject(Router);
+
+  @Output() closeForm = new EventEmitter<boolean>();
+
+  closeNewBoardForm() {
+    this.closeForm.emit(false);
+  }
+
+  form = this.formBuilder.nonNullable.group({
+    title: [''],
+    backgroundColor: new FormControl<Colors>('sky', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  onSaveForm() {
+    if (this.form.valid) {
+      const { title, backgroundColor } = this.form.value;
+      if (title && backgroundColor) {
+        this.boardsService.createBoard(title, backgroundColor).subscribe({
+          next: (board) => {
+            this.router.navigate([`/boards/${board.id}`]);
+            this.closeNewBoardForm();
+            this.form.reset();
+          },
+          error: (err) => {
+            console.error('Error creating board:', err);
+          },
+        });
+      }
+    }
+  }
+}
