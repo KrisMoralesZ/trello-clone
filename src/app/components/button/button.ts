db3a172301311b7a4ac469664d4d803920f55a74
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-07 12:15:44 -0700

    Add Color Button

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { COLORS, Colors } from '@models/colors.model';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.html',
 })
 export class Button {
   @Input() typeButton: 'button' | 'reset' | 'submit' = 'button';
   @Input() loading = false;
+  @Input() color: Colors = 'primary';
+
+  private mapColors = COLORS;
+
+  get colors() {
+    const colors = this.mapColors[this.color];
+    if (colors) {
+      return colors;
+    }
+    return {};
+  }
 }
