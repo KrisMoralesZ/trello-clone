b5d17e74cc2b811514e9c50ffacf1caced9c5003
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-08 10:26:05 -0700

    Add Gradient Colors

--- src/app/models/colors.model.ts
+++ src/app/models/colors.model.ts
@@ -72,34 +72,34 @@ export const COLORS: ObjectColors = {
 
 export const BACKGROUNDS: ObjectColors = {
   success: {
-    'bg-success-600': true,
+    'bg-gradient-to-br from-emerald-400 via-emerald-500 to-green-600': true,
   },
   primary: {
-    'bg-primary-600': true,
+    'bg-gradient-to-br from-blue-500 via-indigo-500 to-indigo-700': true,
   },
   danger: {
-    'bg-danger-600': true,
+    'bg-gradient-to-br from-red-400 via-rose-500 to-pink-600': true,
   },
   light: {
-    'bg-gray-600': true,
+    'bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400': true,
   },
   sky: {
-    'bg-sky-600': true,
+    'bg-gradient-to-br from-sky-400 via-sky-500 to-blue-600': true,
   },
   yellow: {
-    'bg-yellow-600': true,
+    'bg-gradient-to-br from-amber-300 via-amber-400 to-yellow-500': true,
   },
   green: {
-    'bg-green-600': true,
+    'bg-gradient-to-br from-green-400 via-emerald-500 to-teal-600': true,
   },
   red: {
-    'bg-red-600': true,
+    'bg-gradient-to-br from-red-400 via-rose-500 to-pink-600': true,
   },
   violet: {
-    'bg-violet-400': true,
+    'bg-gradient-to-br from-violet-400 via-purple-500 to-indigo-600': true,
   },
   gray: {
-    'bg-gray-600': true,
+    'bg-gradient-to-br from-gray-500 via-gray-600 to-gray-700': true,
   },
 };
 
